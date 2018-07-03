f23d35b503c18278c44800caa39c5e3ffc709db9
dengzhenzhen <zhenzhendeng2012@gmail.com>
2018-07-03 14:41:59 +0800

    增加浮点数除法

--- calculate.py
+++ calculate.py
@@ -1,67 +1,59 @@
 import re
 
-def calculate(operation):
-    result = 0
-    sumlist = operation.replace('-','+-').split('+')
-    for i in sumlist:
-        if i == '':
-            continue
-        else:
-            result += mutidiv(i)
-    return result
-
-
-def mutidiv(s):
-
-    if '*' not in s and '//' not in s:
-        return int(s)
-    elif '*' in s:
-        return mutidiv(s.split('*',1)[0]) * mutidiv(s.split('*',1)[1])
+#-----------找到当前的优先运算符--------------------------------------
+def FindPrioritizedOpetator(operators):
+    if len(set(['*','/','//']) & set(operators) ) > 0:
+        for i in operators:
+            if i in ['*','/','//']:
+                return {'index':operators.index(i),'operator':i}
+            else:
+                continue
     else:
-        return mutidiv(s.split('//',1)[0]) // mutidiv(s.split('//',1)[1])
+        return {'index':0,'operator':operators[0]}
+
+#-------------------------------------------------------------------
 
 
+def calculate(operation):
 
-def calculate_(s):
-    flag = re.compile('[\+\-\*]|//')
-    operands = flag.split(s)
-    operators = flag.findall(s)
+    #把输入的算式分割成operator和operand,分别用两个列表存储
+    flag = re.compile('[\+\-\*]|/{1,2}')
+    operation = operation.replace(' ','')
+    operands = flag.split(operation)
+    operators = flag.findall(operation)
 
+    #把所有operand变为浮点数先
     for i in range(len(operands)):
         if operands[i] != '':
-            operands[i] = int(operands[i])
+            operands[i] = float(operands[i])
         else:
             operands[i] = 0
 
+    #计算当前最优先的operator,结果存入参与运算的两个operand的前一个,运算完后把完成运算的运算符和后一个operand去掉,得到一对新的operand和operator列表,继续循环，当列表只剩一个operand时，就是结果
     while(len(operands)>1):    
-        if '*' in operators and '//' in operators:
-            if operators.index('*') < operators.index('//'):
-                operands[operators.index('*')] = operands[operators.index('*')] * operands[operators.index('*') + 1]
-                operands.pop(operators.index('*') + 1)
-                operators.remove('*')
-            else:
-                operands[operators.index('//')] = operands[operators.index('//')] // operands[operators.index('//') + 1]
-                operands.pop(operators.index('//') + 1)
-                operators.remove('//')
-        elif '*' in operators and '//' not in operators:
-            operands[operators.index('*')] = operands[operators.index('*')] * operands[operators.index('*') + 1]
-            operands.pop(operators.index('*') + 1)
-            operators.remove('*')
-        elif '*' not in operators and '//' in operators:
-            operands[operators.index('//')] = operands[operators.index('//')] // operands[operators.index('//') + 1]
-            operands.pop(operators.index('//') + 1)
-            operators.remove('//')
-        else:
-            if operators[0] == '+':
-                operands[0] += operands[1]
-                operands.pop(1)
-            else:
-                operands[0] -= operands[1]
-                operands.pop(1)
 
+        CurrentStep = FindPrioritizedOpetator(operators)
+
+        if CurrentStep['operator'] == '+':
+            operands[CurrentStep['index']] += operands[CurrentStep['index'] + 1]
+
+        elif CurrentStep['operator'] == '-':
+            operands[CurrentStep['index']] -= operands[CurrentStep['index'] + 1]
+
+        elif CurrentStep['operator'] == '*':
+            operands[CurrentStep['index']] *= operands[CurrentStep['index'] + 1]
+
+        elif CurrentStep['operator'] == '/':
+            operands[CurrentStep['index']] /= operands[CurrentStep['index'] + 1]
+
+        else:
+            operands[CurrentStep['index']] //= operands[CurrentStep['index'] + 1]
+
+        operands.pop(CurrentStep['index'] + 1)
+        operators.pop(CurrentStep['index'])
     return operands[0]
 
-
-
-
-
+#test
+s = input()
+print(calculate(s))
+print(eval(s))
